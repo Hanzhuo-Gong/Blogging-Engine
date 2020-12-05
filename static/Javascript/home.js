9792f8dd800b7030e2902f1d19f84eee4277ee8b
Hanzhuo-Gong <hgong@scu.edu>
2020-12-04 22:57:00 -0800

    get request on home.js working. Will fix styling problem

--- static/Javascript/home.js
+++ static/Javascript/home.js
@@ -15,7 +15,49 @@ window.onload = () => {
   fetch('/api/get')
     .then((response) => (response.ok ? response.json() : Promise.reject()))
     .then((data) => {
-      console.log("get working");
       console.log(data);
+      console.log(data[0].title);
+
+      //check if the database is empty, if is empty, display no content
+      if (data.length === 0) {
+        //<h3 class="no-content">No Blog Content</h3>
+        const newH3 = document.createElement("h3");
+        const h3Content = document.createTextNode("No Blog Content");
+        newH3.classList.add("no-content");
+
+        newH3.appendChild(h3Content);
+        allBlogs.appendChild(newH3);     //Need to come back and test later~~~~~~~
+      }
+      else {
+        //After getting the data from database, need to create new element to display the content
+        const titleDiv = document.createElement("div");
+        const contentDiv = document.createElement("div");
+
+        const newATag = document.createElement("a");
+        const newH3 = document.createElement("h3");
+        const h3Content = document.createTextNode(data[0].title);
+        const newParagraph = document.createElement("p");
+        const paragraphContent = document.createTextNode(data[0].content);
+
+
+        newATag.href = "#";
+        newATag.classList.add("title-content");
+        
+        //append the blog title
+        allBlogs.appendChild(titleDiv);
+        newH3.appendChild(h3Content);
+        newATag.appendChild(newH3);
+        titleDiv.appendChild(newATag);
+
+        //append the blog content
+        allBlogs.appendChild(contentDiv);
+        newParagraph.appendChild(paragraphContent);
+        contentDiv.appendChild(newParagraph);
+
+
+        //add the href to all the
+      }
+
+
     })
 }
